dc12576e37736463d6e5bdb577fe251ce976c354
subhradipWeb <113184333+subhradipWeb@users.noreply.github.com>
2023-06-05 23:28:45 +0530

    add connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,28 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/Habit_db');
-const db = mongoose.connection;
+// const mongoose = require("mongoose");
+// const connectDB = async() => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log(`Mongodb connected ${mongoose.connection.host}`);
+//     } catch (error) {
+//         console.log(`Mongodb Server Issue ${error}`);
+//     }
+// };
 
-db.on('error', console.error.bind(console, 'erroe connecting to db'));
+// module.exports = connectDB;const mongoose = require("mongoose");
 
-db.once('open', function() {
-    console.log('Success fully connected to the database')
-})
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(
+        "mongodb+srv://subhradipsamanta99:LhFqcpRfazUpNZaP@cluster0.iurywf4.mongodb.net/?retryWrites=true&w=majority", {
+            // useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        console.log("connection is successfully");
+    })
+    .catch((e) => {
+        console.log("No Connection");
+    });
